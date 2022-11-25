1e6319e5b195c1cd1a493f8fd33398d0f27a9146
Jaeyoon Lee <jylee@4dreplay.com>
2022-11-25 11:17:33 +0900

    [fix] configuration reformat using prettier

--- config/configuration.ts
+++ config/configuration.ts
@@ -27,8 +27,7 @@ export default () => ({
     password: process.env.DATABASE_PASSWORD as string,
     database: process.env.DATABASE_NAME as string,
     entities: [__dirname + "/../**/*.entity{.ts,.js}"],
-    synchronize:
-      JSON.parse(process.env.DATABASE_SYNCHRONIZE as string) || false,
+    synchronize: JSON.parse(process.env.DATABASE_SYNCHRONIZE as string) || false,
     logging: JSON.parse(process.env.DATABASE_LOGGING as string) || true,
     charset: process.env.DATABASE_CHARSET as string,
     timezone: (process.env.DATABASE_TIMEZONE as string) || "+09:00",
