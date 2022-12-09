e323eed015a1d311297ae61c8baeb824edae1cd5
Jaeyoon Lee <jylee@4dreplay.com>
2022-12-09 14:35:07 +0900

    [fix] change process.title, add server initial time logs

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { setupSwagger } from "./libs/utils/swagger";
 import { HttpExceptionFilter } from "./libs/utils/globalErrorHandler";
 
 async function bootstrap() {
+  const startTime = process.hrtime();
   const app = await NestFactory.create(AppModule, {});
+
+  process.title = process.env.HOSTNAME;
+
   setupSwagger(app);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.SERVER_PORT);
+  const serverStart = await app.listen(process.env.SERVER_PORT);
+  if (serverStart) {
+    setTimeout(() => {
+      const endTime = process.hrtime(startTime);
+      console.log(`SERVER START in %d.%d seconds`, endTime[0], endTime[1]);
+    }, 1000);
+  }
 }
 void bootstrap();
